3ea88d0e293e527c188205fa761d6ae540705df6
joaquin-villar <joaquinvillar8802@gmail.com>
2025-11-06 15:24:12 +0000

    Update site content

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Joaquin Villar</h1>
+        <p>Your Title/Role</p>
+      </header>
+
+      <section className="about">
+        <h2>About Me</h2>
+        <p>Your bio here...</p>
+      </section>
+
+      <section className="projects">
+        <h2>Projects</h2>
+        {/* Your projects here */}
+      </section>
+    </div>
+  );
+}
+
+export default App;
